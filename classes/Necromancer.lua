3830e8042803860bf12c8e42c0d6fe2a9f28d4ae
Skuyou <47137412+hachigen@users.noreply.github.com>
2023-06-13 06:56:04 -0700

    First upload

---
+++ classes/Necromancer.lua
@@ -0,0 +1,44 @@
+{
+    classShortName = "NEC",
+    className = "Necromancer",
+    Invisibility = {
+        {
+            name = "Invisibility",
+            activation = {
+                ability = "cast Invisibility",
+                level = 8,
+                type = "spell"
+            }
+        }
+    },
+    ivu = {
+        {
+            name = "IvU Evade",
+            activation = {
+                ability = "IvU Evade",
+                level = 12,
+                type = "ability"
+            }
+        }
+    },
+    evade = {
+        {
+            name = "Evade",
+            activation = {
+                ability = "Evade",
+                level = 6,
+                type = "ability"
+            }
+        }
+    },
+    sos = {
+        {
+            name = "SOS",
+            activation = {
+                ability = "SOS",
+                level = 18,
+                type = "ability"
+            }
+        }
+    }
+}
---
+++ classes/classes.lua
@@ -0,0 +1,514 @@
+local class_info = {
+    -- Demo Class
+    {
+        classShortName = "DEM",
+        className = "Demo Class",
+        invisAbility = {
+            "null"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Invisible1",
+            "Invisible2",
+            "Invisible3"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Sos1",
+            "Sos2",
+            "Sos3"
+        },
+        activationSos = {
+            "Activation21"
+        },
+        evade = {},
+        activationEvade = {},
+        evadeActivation = {}
+    },
+    -- Class 1: Berserker
+    {
+        classShortName = "BER",
+        className = "Berserker",
+        invisAbility = {
+            "null"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "null"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Wild"
+        },
+        activationSos = {
+            "Activation18"
+        },
+        evade = {
+            "Evade1",
+            "Evade2",
+            "Evade3"
+        },
+        activationEvade = {
+            "EvadeActivation1"
+        },
+        evadeActivation = {}
+    },
+    -- Class 2: Bard
+    {
+        classShortName = "BRD",
+        className = "Bard",
+        invisAbility = {
+            "Melody of Ervaj"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Song of Stealth"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Hero"
+        },
+        activationSos = {
+            "Activation3"
+        },
+        evade = {
+            "Evade4",
+            "Evade5",
+            "Evade6"
+        },
+        activationEvade = {
+            "EvadeActivation2"
+        },
+        evadeActivation = {}
+    },
+    -- Class 3: Cleric
+    {
+        classShortName = "CLR",
+        className = "Cleric",
+        invisAbility = {
+            "Divine Aura"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Divine Stealth"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Divine Intervention"
+        },
+        activationSos = {
+            "Activation5"
+        },
+        evade = {
+            "Evade7",
+            "Evade8",
+            "Evade9"
+        },
+        activationEvade = {
+            "EvadeActivation3"
+        },
+        evadeActivation = {}
+    },
+    -- Class 4: Druid
+    {
+        classShortName = "DRU",
+        className = "Druid",
+        invisAbility = {
+            "Camouflage"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Stealth of the Wood"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Nature's Call"
+        },
+        activationSos = {
+            "Activation9"
+        },
+        evade = {
+            "Evade10",
+            "Evade11",
+            "Evade12"
+        },
+        activationEvade = {
+            "EvadeActivation4"
+        },
+        evadeActivation = {}
+    },
+    -- Class 5: Enchanter
+    {
+        classShortName = "ENC",
+        className = "Enchanter",
+        invisAbility = {
+            "Invisibility"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Invisible"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Hero"
+        },
+        activationSos = {
+            "Activation15"
+        },
+        evade = {
+            "Evade13",
+            "Evade14",
+            "Evade15"
+        },
+        activationEvade = {
+            "EvadeActivation5"
+        },
+        evadeActivation = {}
+    },
+    -- Class 6: Magician
+    {
+        classShortName = "MAG",
+        className = "Magician",
+        invisAbility = {
+            "Invisibility"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Invisible"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Hero"
+        },
+        activationSos = {
+            "Activation16"
+        },
+        evade = {
+            "Evade16",
+            "Evade17",
+            "Evade18"
+        },
+        activationEvade = {
+            "EvadeActivation6"
+        },
+        evadeActivation = {}
+    },
+    -- Class 7: Monk
+    {
+        classShortName = "MNK",
+        className = "Monk",
+        invisAbility = {
+            "Sneak"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Hide"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Hero"
+        },
+        activationSos = {
+            "Activation10"
+        },
+        evade = {
+            "Evade19",
+            "Evade20",
+            "Evade21"
+        },
+        activationEvade = {
+            "EvadeActivation7"
+        },
+        evadeActivation = {}
+    },
+    -- Class 8: Necromancer
+    {
+        classShortName = "NEC",
+        className = "Necromancer",
+        invisAbility = {
+            "Invisibility"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Invisible"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Hero"
+        },
+        activationSos = {
+            "Activation14"
+        },
+        evade = {
+            "Evade22",
+            "Evade23",
+            "Evade24"
+        },
+        activationEvade = {
+            "EvadeActivation8"
+        },
+        evadeActivation = {}
+    },
+    -- Class 9: Paladin
+    {
+        classShortName = "PAL",
+        className = "Paladin",
+        invisAbility = {
+            "Divine Aura"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Divine Stealth"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Divine Intervention"
+        },
+        activationSos = {
+            "Activation6"
+        },
+        evade = {
+            "Evade25",
+            "Evade26",
+            "Evade27"
+        },
+        activationEvade = {
+            "EvadeActivation9"
+        },
+        evadeActivation = {}
+    },
+    -- Class 10: Ranger
+    {
+        classShortName = "RNG",
+        className = "Ranger",
+        invisAbility = {
+            "Camouflage"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Stealth of the Wood"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Nature's Call"
+        },
+        activationSos = {
+            "Activation7"
+        },
+        evade = {
+            "Evade28",
+            "Evade29",
+            "Evade30"
+        },
+        activationEvade = {
+            "EvadeActivation10"
+        },
+        evadeActivation = {}
+    },
+    -- Class 11: Rogue
+    {
+        classShortName = "ROG",
+        className = "Rogue",
+        invisAbility = {
+            "Sneak"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Hide"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Hero"
+        },
+        activationSos = {
+            "Activation12"
+        },
+        evade = {
+            "Evade31",
+            "Evade32",
+            "Evade33"
+        },
+        activationEvade = {
+            "EvadeActivation11"
+        },
+        evadeActivation = {}
+    },
+    -- Class 12: Shadowknight
+    {
+        classShortName = "SHD",
+        className = "Shadowknight",
+        invisAbility = {
+            "Shadowstep"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Cloak of Shadows"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Death's Door"
+        },
+        activationSos = {
+            "Activation8"
+        },
+        evade = {
+            "Evade34",
+            "Evade35",
+            "Evade36"
+        },
+        activationEvade = {
+            "EvadeActivation12"
+        },
+        evadeActivation = {}
+    },
+    -- Class 13: Shaman
+    {
+        classShortName = "SHM",
+        className = "Shaman",
+        invisAbility = {
+            "Invisibility"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Invisible"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Spirit of the Wolf"
+        },
+        activationSos = {
+            "Activation11"
+        },
+        evade = {
+            "Evade37",
+            "Evade38",
+            "Evade39"
+        },
+        activationEvade = {
+            "EvadeActivation13"
+        },
+        evadeActivation = {}
+    },
+    -- Class 14: Warrior
+    {
+        classShortName = "WAR",
+        className = "Warrior",
+        invisAbility = {
+            "null"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "null"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "null"
+        },
+        activationSos = {
+            "Activation20"
+        },
+        evade = {
+            "Evade40",
+            "Evade41",
+            "Evade42"
+        },
+        activationEvade = {
+            "EvadeActivation14"
+        },
+        evadeActivation = {}
+    },
+    -- Class 15: Wizard
+    {
+        classShortName = "WIZ",
+        className = "Wizard",
+        invisAbility = {
+            "Invisibility"
+        },
+        activationInvis = {
+            "Invisibility Activation"
+        },
+        ivuAbility = {
+            "Invisible"
+        },
+        activationIvu = {
+            "Activation20"
+        },
+        sosAbility = {
+            "Call of the Hero"
+        },
+        activationSos = {
+            "Activation13"
+        },
+        evade = {
+            "Evade43",
+            "Evade44",
+            "Evade45"
+        },
+        activationEvade = {
+            "EvadeActivation15"
+        },
+        evadeActivation = {}
+    }
+}
+
+return class_info
